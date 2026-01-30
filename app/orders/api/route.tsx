772675490fa193954d9b061f10ec31d78fb38c55
cristianrivas007 <cristianrivas0071@gmail.com>
2026-01-30 17:01:13 -0500

    fix: remove duplicate dynamic export

--- app/orders/api/route.tsx
+++ app/orders/api/route.tsx
@@ -2,7 +2,7 @@ export const dynamic = 'force-dynamic'
 
 import { prisma } from "@/src/lib/prisma";
 
-export const dynamic = 'force-dynamic'
+
 
 export async function GET() {
     const orders = await prisma.order.findMany({
